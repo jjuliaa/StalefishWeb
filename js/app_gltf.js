da8643ca38b69e293509c1b5c1b5e815b91867a7
jjuliaa <julialc4@gmail.com>
2021-09-04 13:31:17 -0700

    switch to non-modules CDN loading
    modules got too involved over gh-pages.. switching to old version of cdn loading style

--- js/app_gltf.js
+++ js/app_gltf.js
@@ -1,6 +1,6 @@
-      import * as THREE from 'https://unpkg.com/browse/three@0.132.2/build/three.module.js';
-      import { GLTFLoader } from 'https://unpkg.com/browse/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
-      import { OrbitControls } from 'https://unpkg.com/browse/three@0.132.2/examples/jsm/controls/OrbitControls.js';
+      //import * as THREE from 'https://unpkg.com/browse/three@0.132.2/build/three.module.js';
+      //import { GLTFLoader } from 'https://unpkg.com/browse/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+      //import { OrbitControls } from 'https://unpkg.com/browse/three@0.132.2/examples/jsm/controls/OrbitControls.js';
 
       var canvas;
 
